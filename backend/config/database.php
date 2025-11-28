c46a5872c1fb9368ff8cae26ab1faa46d68d0852
alyfazeha <alyfazeha@gmail.com>
2025-11-28 07:27:25 +0700

    perbaikan struktur folder (pemisahan frontend dan backend)

---
+++ backend/config/database.php
@@ -0,0 +1,36 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $port = "5432";
+    private $db_name = "pbl";
+    private $username = "postgres";
+    private $password = "lovie180906";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+
+            // Mode error PDO
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // Mengembalikan dalam bentuk associative array
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        } catch(PDOException $e) {
+            http_response_code(500);
+            echo json_encode([
+                "status" => "error",
+                "message" => "Database connection failed",
+                "error" => $e->getMessage()
+            ]);
+            exit;
+        }
+
+        return $this->conn;
+    }
+}
+?>
